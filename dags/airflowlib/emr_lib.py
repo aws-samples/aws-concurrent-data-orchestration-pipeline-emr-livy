b0da24d3286b18cb1c54e0e0dbe5746c6a3da0ae
bjhaveri <bjhaveri@amazon.com>
2018-07-10 13:13:17 -0700

    Added EMR jobrole and servicerole

--- dags/airflowlib/emr_lib.py
+++ dags/airflowlib/emr_lib.py
@@ -15,7 +15,7 @@ def get_security_group_id(group_name, region_name):
     response = ec2.describe_security_groups(GroupNames=[group_name])
     return response['SecurityGroups'][0]['GroupId']
 
-def create_cluster(region_name, cluster_name='Airflow-' + str(datetime.now()), release_label='emr-5.9.0',master_instance_type='m3.xlarge', num_core_nodes=2, core_node_instance_type='m3.2xlarge'):
+def create_cluster(region_name, cluster_name='Airflow-' + str(datetime.now()), release_label='emr-5.9.0',master_instance_type='m5.xlarge', num_core_nodes=2, core_node_instance_type='m5.2xlarge'):
     emr_master_security_group_id = get_security_group_id('AirflowEMRMasterSG', region_name=region_name)
     emr_slave_security_group_id = get_security_group_id('AirflowEMRSlaveSG', region_name=region_name)
     cluster_response = emr.run_job_flow(
@@ -44,8 +44,8 @@ def create_cluster(region_name, cluster_name='Airflow-' + str(datetime.now()), r
             'EmrManagedSlaveSecurityGroup': emr_slave_security_group_id
         },
         VisibleToAllUsers=True,
-        JobFlowRole='EMR_EC2_DefaultRole',
-        ServiceRole='EMR_DefaultRole',
+        JobFlowRole='EmrEc2InstanceProfile',
+        ServiceRole='EmrRole',
         Applications=[
             { 'Name': 'hadoop' },
             { 'Name': 'spark' },
